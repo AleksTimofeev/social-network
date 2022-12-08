2859e1fc9a0b5c07563911857b4c6eb4a279cd60
AleksTimofeev <timoha144a@gmail.com>
2022-12-08 23:59:40 +0300

    ...

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,13 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import {Grid} from "@mui/material";
+import styles from './Profile.module.css'
+import avatar from '../../assets/image/artworks-SFJCSqkDi8QduppR-9TFs6Q-t500x500.jpg'
+import {useAppSelector} from "../../app/store";
 
 export const Profile = () => {
 
+  const status = useAppSelector(state => state.profile.status)
+  const photo = useAppSelector(state => state.profile.profileData.photos.large)
+  const contacts = useAppSelector(state => state.profile.profileData.contacts)
+
   return (
     <Grid container spacing={2}>
-      <Grid item xs={8}>
-          <h2>Profile</h2>
-      </Grid>
+      <div className={styles.container}>
+        <div className={styles.photo}>
+          <img src={photo ? photo : avatar} alt={'avatar'} />
+        </div>
+        <div className={styles.items}>
+          {Object.entries(contacts).map(item => (
+            <span>{item[0]} - {item[1] ? item[1] : 'not information'}</span>
+          ))}
+        </div>
+        <span>{status}</span>
+      </div>
     </Grid>
   );
 }
