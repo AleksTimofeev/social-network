d640f0cad4250da78cb481193062f8f4229dc4ad
AleksTimofeev <timoha144a@gmail.com>
2022-12-10 04:58:02 +0300

    add usersList pagination

--- src/features/users/Users.tsx
+++ src/features/users/Users.tsx
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {useAppDispatch, useAppSelector} from "../../app/store";
 import {getUsers} from "./usersReducer";
+import styles from './Users.module.css'
+import {Pagination} from "antd";
+import {log} from "util";
 
 export const Users = () => {
 
@@ -11,13 +14,26 @@ export const Users = () => {
   const pageCount = Math.ceil(dataUsers.totalCount / countUsers)
   console.log('page count - ' + pageCount)
 
+  const handleChangeCurrentPage = (page: number, pageSize: number) => {
+    setPage(page)
+  }
+
+  const handleChangeSizePage = (current: number, size: number) => {
+    setCountUsers(size)
+  }
+
   useEffect(() => {
     dispatch(getUsers({page, countUsers}))
   },[page, countUsers])
 
   return (
-    <div>
-
+    <div className={styles.wrapper}>
+      <Pagination
+        current={page}
+        total={dataUsers.totalCount}
+        onChange={handleChangeCurrentPage}
+        onShowSizeChange={handleChangeSizePage}
+      />
     </div>
   );
 };
