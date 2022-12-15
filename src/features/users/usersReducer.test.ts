855574f306e78d96c2f2d29fc36dd9d50f8c2c8b
AleksTimofeev <timoha144a@gmail.com>
2022-12-15 23:27:44 +0300

    add usersReducer test

---
+++ src/features/users/usersReducer.test.ts
@@ -0,0 +1,75 @@
+import {UsersType} from "../../api/api";
+import {follow, getUsers, unfollow, usersReducer} from "./usersReducer";
+
+
+let data: UsersType & {followStatus: Array<number>}
+
+beforeEach(() => {
+  data = {
+    items: [
+      {
+        name: 'alex',
+        id: 1,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: 'status',
+        followed: false,
+      },
+      {
+        name: 'sofia',
+        id: 2,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: 'status sofia',
+        followed: false,
+      },
+      {
+        name: 'olga',
+        id: 3,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: 'status olga',
+        followed: false,
+      },
+    ],
+    totalCount: 155,
+    followStatus: [],
+    error: null
+  }
+})
+
+test('get users', () => {
+  const initialState = {
+    items: [],
+    totalCount: 0,
+    error: null,
+    followStatus: []
+  }
+  const action = getUsers.fulfilled(
+    {items: data.items, error: null, totalCount: data.totalCount}, '', {countUsers: 10, page: 1})
+  const newState = usersReducer(initialState, action)
+
+  expect(newState.items).toEqual(data.items)
+})
+test('follow/unfollow', () => {
+  const action = follow.fulfilled(3, '', {userId: 3})
+  const newState = usersReducer(data, action)
+  expect(newState.items[2].followed).toBe(true)
+
+  const action1 = follow.fulfilled(2, '', {userId: 2})
+  const newState1 = usersReducer(newState, action1)
+  expect(newState1.items[1].followed).toBe(true)
+
+  const action2 = unfollow.fulfilled(2, '', {userId: 2})
+  const newState2 = usersReducer(newState1, action2)
+  expect(newState2.items[1].followed).toBe(false)
+})
